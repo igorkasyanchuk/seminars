51875f0a6e9db4104422c3f0b96585ec63042082
Igor Kasyanchuk <igorkastyanchuk@gmail.com>
2012-02-07 13:58:53 -0800

    migrate

--- db/migrate/20120207191040_add_start_to_panel.rb
+++ db/migrate/20120207191040_add_start_to_panel.rb
@@ -1,5 +1,8 @@
 class AddStartToPanel < ActiveRecord::Migration
-  def self.change
+  def self.up
     add_column :panels, :start_at, :datetime
+  end
+  def self.down
+    remove_column :panels, :start_at
   end
 end
