ecf2a22867f923045ddb8e200bd1abe7d8f8266c
root <root@ubuntu.localdomain>
2012-02-13 12:30:34 -0800

    add resource ui

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,5 @@ class Document < ActiveRecord::Base
   belongs_to :seminar
   has_many :attachments	
   accepts_nested_attributes_for :attachments, :allow_destroy => true, :reject_if => :all_blank
+  validates_presence_of :title
 end
